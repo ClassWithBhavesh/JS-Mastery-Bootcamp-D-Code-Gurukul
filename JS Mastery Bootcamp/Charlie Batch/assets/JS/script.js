5516f6e87c2b1343bce958db8a2c8c96c62cd27a
Bhavesh Joshi <classwithbhavesh@gmail.com>
2026-01-22 18:37:32 +0530

    Event handling and string methods covered

--- JS Mastery Bootcamp/Charlie Batch/assets/JS/script.js
+++ JS Mastery Bootcamp/Charlie Batch/assets/JS/script.js
@@ -786,3 +786,241 @@
 // })
 
 // console.log(result);
+
+
+
+
+
+
+// Stirngs Methods - 
+
+// // 1. Length :
+// var str = "This is a JavaScript Tutorial.";
+// document.write(str + "<br><br>");
+// var l = str.length;
+// document.write(l + "<br><br>");
+
+// // 2. toLowerCase :
+// var str = "Let's See the SECOND String Method";
+// document.write(str + "<br><br>");
+// var l = str.toLowerCase();
+// document.write(l + "<br><br>");
+
+// // 3. toUpperCase :
+// var str = "let's see the third string method";
+// document.write(str + "<br>");
+// var u = str.toUpperCase();
+// document.write(u + "<br>");
+
+// // 4. includes :
+// var str = "let's see the fourth string method";
+// var i = str.includes("let's see");
+// document.write(i + "<br>");
+
+// // 5. startsWith :
+// var str = "let's see the fifth string method";
+// var i = str.startsWith("le");
+// document.write(i + "<br>"); 
+
+// // 6. endsWith :
+// var str = "let's see the sixth string method!";
+// var i = str.endsWith("method");
+// document.write(i + "<br>");
+
+
+
+// // 7. search :
+// var str = "let's see the sixth string method";
+// var i = str.search("s");
+// document.write(i + "<br>");
+
+// // 8. match :
+// var str = "let's see the eigth string method";
+// var i = str.match(/z/g);
+// var i = str.match("z");
+// document.write(i + "<br>");
+// document.write(typeof(i));
+// console.log(i);
+
+// // 9. indexOf :
+// var str = "let's see the ninth string method";
+// var i = str.indexOf("s", 10);
+// document.write(i + "<br>");
+
+// // 10. lastIndexOf :
+// var str = "let's see the ninth string method";
+// var i = str.lastIndexOf("h", 25);
+// document.write(i + "<br>");
+
+// // 11. replace :
+// var str = "let's see the ninth see string see method see";
+// document.write(str + "<br>");
+// // var i = str.replace("see", "move"); // (// can be used for more then one replace)
+// var i = str.replace(/see/g, "move"); // (// can be used for more then one replace)
+// document.write(i + "<br>");
+
+
+
+// // 12. trim :
+// var str = "      Hello       Students";
+// var str1 = "      Hello       Students";
+// document.write(str);
+// console.log(str);
+// document.write(str);
+// var a = str.trim();
+// console.log(a);
+// console.log(str.concat(str1))
+
+// // 13. charAt :
+// var str = "let's see the ninth string method";
+// var i = str.charAt(11); // (// can be used for more then one replace)
+// document.write(i + "<br>");
+
+// // 14. charCodeAt :
+// var str = "let's see the ninth string method";
+// var i = str.charCodeAt(32);
+// document.write(i + "<br>");
+
+// var i = String.fromCharCode(67);
+// document.write(i + "<br>"); 
+
+// 15. concat  :
+// var str = "I'm a student";
+// var s = " and i belong to Indore";
+// var a = str.concat(s);
+// document.write(a + "<br>");
+
+
+
+
+// // 16. split :
+// var str = "JavaScript is a intresting language";
+// var a = str.split("h");
+// document.write(a + "<br>");
+
+// // 17. repeat :
+// var str = "JavaScript is a intresting language";
+// var a = str.repeat(3);
+// document.write(a + "<br>");
+
+// // 18. slice :
+// var str = "JavaScript is a intresting language";
+// // var a = str.slice(10);
+// var a = str.slice(10, 20);
+// document.write(a + "<br>");
+
+// // 19. substr :
+// var str = "JavaScript is a intresting language";
+// var a = str.substr(3,10);
+// document.write(a + "<br>");
+
+// // // 20. substring :
+// var str = "JavaScript is a intresting language";
+// var a = str.substring(3,12);
+// document.write(a + "<br>");
+
+// // 21. toString :
+// var st = 25014;
+// var str = st.toString();
+// document.write(str + "<br>");
+// document.write(typeof(str));
+
+
+
+
+
+
+// "DOM Targeting Methods" -
+
+// let divEle = document.getElementsByTagName("div");
+// console.log(divEle);
+
+
+// let classEle = document.getElementsByClassName("container");
+// console.log(classEle);
+
+
+// let uniqueEle = document.getElementById("cont1");
+// console.log(uniqueEle);
+
+
+
+// let tagEle = document.querySelector("div");
+// console.log(tagEle);
+
+
+// let classEle = document.querySelector(".container");
+// console.log(classEle);
+
+
+// let unqiueEle = document.querySelector("#cont3");
+// console.log(unqiueEle);
+
+
+// let unqiueEle = document.querySelectorAll("#cont");
+// console.log(unqiueEle);
+
+
+// let unqiueEle = document.querySelectorAll(".container");
+// console.log(unqiueEle);
+
+
+
+// Event Handling -
+
+function show_message(){
+    alert("Hello From JavaScript");
+}
+
+let btn = document.querySelector("#btn");
+// btn.ondblclick = show_message;
+
+
+// btn.addEventListener("click", () => {
+//     alert("On Click Event Triggered!");
+// })
+
+
+// btn.addEventListener("dblclick", () => {
+//     alert("On Click Event Triggered!");
+// })
+
+
+// btn.addEventListener("contextmenu", () => {
+//     alert("On Click Event Triggered!");
+// })
+
+
+// btn.addEventListener("mouseenter", () => {
+//     alert("On Click Event Triggered!");
+// })
+
+
+// btn.addEventListener("mouseleave", () => {
+//     alert("On Click Event Triggered!");
+// })
+
+
+// document.addEventListener("keypress", () => {
+//     alert("On Click Event Triggered!");
+// })
+
+
+// document.addEventListener("keyup", () => {
+//     alert("On Click Event Triggered!");
+// })
+
+
+// document.addEventListener("keydown", () => {
+//     alert("On Click Event Triggered!");
+// })
+
+
+// document.addEventListener("scroll", () => {
+//     alert("On Click Event Triggered!");
+// })
+
+
+// document.addEventListener("load", () => {
+//     alert("On Click Event Triggered!");
+// })
